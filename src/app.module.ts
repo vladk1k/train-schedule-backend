dc1a86d2a32a1582216653ffbf8e003e984b9b5e
Vlad Kiktiev <vlad@carbonatix.com>
2025-10-03 16:21:10 +0200

    add dev db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
-import { AuthModule } from './auth/auth.module';
 import { Schedule } from './schedules/schedule.entity';
+import { AuthModule } from './auth/auth.module';
 import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'train_user',
-      password: 'train_user',
-      database: 'trainschedule',
+      url: process.env.DATABASE_URL,
       entities: [User, Schedule],
-      synchronize: true,
+      synchronize: true, // dev only!
     }),
     AuthModule,
     SchedulesModule,
