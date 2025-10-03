e5bc606c0a3d67f2152ac05ad25152bf528e46ac
Vlad Kiktiev <vlad@carbonatix.com>
2025-10-03 17:37:36 +0200

    fix url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://train-schedule-frontend.onrender.com',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
